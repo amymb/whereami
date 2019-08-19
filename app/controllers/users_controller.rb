cf0a003f2ec4b873456f6433477a0031b4e68613
amymb <amy.bertken@cognizant.com>
2019-08-19 14:07:19 -0600

    fix typo maybe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            render json: @user, :except => [:password_digest, :updated_at] status: created
+            render json: @user, :except => [:password_digest, :updated_at], status: created
         else 
             render json: { errors: @user.errors.full_messages}, status: unprocessable_entity
         end
