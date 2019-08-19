ef6e09e3c1d2eb8dd600136563e5721586735f9a
amymb <amy.bertken@cognizant.com>
2019-08-19 16:29:57 -0600

    let's see

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,9 @@ module Whereami
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    p config.autoload_paths
-    config.autoload_paths << Rails.root.join('lib')
-    p config.autoload_paths
+    p config.eager_load_paths
+    config.eager_load_paths << Rails.root.join('lib')
+    p config.eager_load_paths
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
